36852937d63468e629935f09ea5975373d7b31bd
ronan <ronan>
2014-11-11 16:47:45 +0800

    Support for dual output reading

--- shinyei-lpo-reader.py
+++ shinyei-lpo-reader.py
@@ -7,75 +7,94 @@ import json
 import httplib, urllib
 
 
+class LpoReader:
+
+	def __init__(self):
+		self.start()
+
+	def start( self ):
+		self.ltime = 0
+		self.lvalue = -1
+		self.lpotime = [0,0]
+
+	def read( self, value ):
+
+		if self.lvalue != value:
+			t = time.time()
+			if self.ltime!=0:
+				dtime = t-self.ltime
+				self.lpotime[value] += dtime
+			self.ltime=t
+			self.lvalue = value
+
+	def ratio( self ):
+		low = self.lpotime[1]
+		high = self.lpotime[0]
+		if (low+high!=0):
+			ratio = float(low) / (low+high)
+		else:
+			ratio = -1
+
+		return ratio	
+
 class ShinyeiLpoReader:
 
-	def __init__(self, sensorId, gpio):
-		self.id=sensorId
-		self.gpio = gpio
-		self.openGpio()
+	def __init__(self, gpio1, gpio2):
+		self.gpio1 = self.openGpio(gpio1)
+		self.gpio2 = self.openGpio(gpio2)
+		self.lpo1 = LpoReader()
+		self.lpo2 = LpoReader()
 
-	def openGpio(self):
-		pinModePath = os.path.join(os.path.normpath('/sys/devices/virtual/misc/gpio/mode/'), self.gpio)
+	def openGpio(self,gpio):
+		pinModePath = os.path.join(os.path.normpath('/sys/devices/virtual/misc/gpio/mode/'), gpio)
 		file = open(pinModePath, 'r+')
 		file.write("0")
 		file.close()
 
-		pinValuePath = os.path.join(os.path.normpath('/sys/devices/virtual/misc/gpio/pin/'), self.gpio)
-		self.gpioFile = open(pinValuePath, 'r')
+		pinValuePath = os.path.join(os.path.normpath('/sys/devices/virtual/misc/gpio/pin/'), gpio)
+		gpioFile = open(pinValuePath, 'r')
+		return gpioFile
 
-
-	def pulse(self,value):
-		t = time.time()
-		if self.ltime!=0:
-			dtime = t-self.ltime
-			self.lpotime[value] += dtime
-			duration = int(dtime*1000);
-#			print("   #{0} -> {1} ms".format(1-value,duration))
-#		else:
-#			print("   #{0} -> starting".format(1-value))
-		self.ltime=t
 
 
 	def read( self, duration ):
-		count = 0
-		while count < 10:
-			low = 0
-			high = 0
-			start = os.times()[4]
+		start = os.times()[4]
 
-			self.ltime = 0
-			self.lpotime = [0,0]
-			lvalue = -1
-			lasttime = start
+		lasttime = start
+		self.lpo1.start()
+		self.lpo2.start()
 
-			while os.times()[4]<start+duration:
-				time.sleep(1.0/1000.0)
-				self.gpioFile.seek(0)
-				value = int(self.gpioFile.read())
+		while os.times()[4]<start+duration:
+			time.sleep(1.0/1000.0)
 
-				if lvalue != value:
-					self.pulse(value)
-					lvalue = value
+			self.gpio1.seek(0)
+			value = int(self.gpio1.read())
+			self.lpo1.read(value)
 
-				low = int(self.lpotime[1]*1000)
-				high = int(self.lpotime[0]*1000)
-				if (low+high!=0):
-					ratio = float(low) / (low+high)
-				else:
-					ratio = -1
+			self.gpio2.seek(0)
+			value = int(self.gpio2.read())
+			self.lpo2.read(value)
 
-				ctime = os.times()[4]
-				if ctime>lasttime+5:
-					lasttime = ctime
-					print ("{0}: {1} {2}+{3}={4}\033[0m".format(time.strftime("%H:%M:%S"),ratio*100,low,high,low+high))
+			ctime = os.times()[4]
+			if ctime>lasttime+5:
+				lasttime = ctime
+				ratio1 = round(self.lpo1.ratio()*10000)/100
+				ratio2 = round(self.lpo2.ratio()*10000)/100
+				print ("{0}: {1} {2}".format(time.strftime("%H:%M:%S"),ratio1,ratio2))
 
-			return ratio
+		return [self.lpo1.ratio(),self.lpo2.ratio()]
 
 
-	def post(self,value):
+class AqicnUploader:
+
+	def __init__(self, sensorId):
+		self.id=sensorId
+
+	def post(self,values):
 		try:
-			idata = dict( value=value, type='shinyei', unit='lpo' )
-			params = urllib.urlencode(dict(postdata=dict( id=self.id, time=datetime.now().isoformat(), data = idata )))                
+			idat1= dict( value=value, type='shinyei-pdp42ns-1u', unit='lpo' )
+			idat2= dict( value=value, type='shinyei-pdp42ns-2.5u', unit='lpo' )
+			params = urllib.urlencode(dict(postdata=dict( id=self.id, time=datetime.now().isoformat(), data = [idat1,idat2])))
 			headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
 			conn = httplib.HTTPConnection("sensor.aqicn.org")
 			conn.request("POST", "/sensor/upload/", params, headers)
@@ -93,11 +112,12 @@ class ShinyeiLpoReader:
 
 
 def loop():
-	reader = ShinyeiLpoReader("shinyei.test-sensor","gpio7")
+	reader = ShinyeiLpoReader("gpio7","gpio6")
+	uploader = AqicnUploader("shinyei.test-sensor")
 	while 1:
 		ratio = reader.read(60)
-		print ("{0}: {1} ---".format(time.strftime("%H:%M:%S"),ratio))
-		reader.post(ratio);
+		print ("--- {0} --- {1} {1} ---".format(time.strftime("%H:%M:%S"),ratio[0],ratio[1]))
+		uploader.post(ratio);
 
 if __name__ == "__main__":
 	loop()
