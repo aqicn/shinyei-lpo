1c1164014ea2bf1c9699ec7a9163fb6da06eb2b1
ronan <ronan>
2014-11-28 13:11:28 +0800

    Fail safe support (keep the date when no Internet connection is available)

--- shinyei-lpo-reader.py
+++ shinyei-lpo-reader.py
@@ -1,10 +1,12 @@
 import os 
-import sys, time, syslog
+import sys, time
 from datetime import datetime, timedelta
 import commands
 import subprocess
 import json
 import httplib, urllib
+import tempfile
+import pickle
 
 
 class LpoReader:
@@ -16,7 +18,7 @@ class LpoReader:
 		self.ltime = 0
 		self.lvalue = -1
 		self.lpotime = [0,0]
-		self.count = 0
+		self.count=0
 
 	def read( self, value ):
 
@@ -28,17 +30,15 @@ class LpoReader:
 			self.ltime=t
 			self.lvalue = value
 			if value==0:
-				self.count += 1
+				self.count+=1
 
-	def getcount( self ):
-		return self.count
 
 	def ratio( self ):
-		low  = self.lpotime[0]
+		low = self.lpotime[0]
 		high = self.lpotime[1]
 
 		if self.ltime!=0:
-			dt = time.time()-self.ltime
+			dt =  time.time()-self.ltime
 			if self.lvalue==0:
 				low += dt
 			else:
@@ -50,6 +50,10 @@ class LpoReader:
 			ratio = -1
 
 		return ratio	
+
+	def getcount( self ):
+		return self.count
+
 
 class ShinyeiLpoReader:
 
@@ -81,6 +85,7 @@ class ShinyeiLpoReader:
 		while os.times()[4]<start+duration:
 			time.sleep(1.0/1000.0)
 
+
 			self.gpio1.seek(0)
 			value = int(self.gpio1.read())
 			self.lpo1.read(value)
@@ -88,69 +93,110 @@ class ShinyeiLpoReader:
 			self.gpio2.seek(0)
 			value = int(self.gpio2.read())
 			self.lpo2.read(value)
-
 			ctime = os.times()[4]
-			if ctime>lasttime+5:
+			if ctime>lasttime+6:
 				lasttime = ctime
 				ratio1 = round(self.lpo1.ratio()*10000)/100
 				ratio2 = round(self.lpo2.ratio()*10000)/100
 				print ("{0}: {1}({3}) {2}({4})".format(time.strftime("%H:%M:%S"),ratio1,ratio2,self.lpo1.getcount(),self.lpo2.getcount()))
 
+
+
 		return [self.lpo1.ratio(),self.lpo2.ratio()]
 
 
-class AqicnUploader:
+class SensorDataUploader:
 
-	def __init__(self, sensorId):
-		self.id=sensorId
+	def __init__(self, id):
+		self.faildate = 0
+		self.writecnt = 0
+		self.id = id
 
-	def post(self,values):
+	def httpPost(self,idata):
 		try:
-			idat1= dict( value=values[0], type='shinyei-pdp42ns-1u', unit='lpo', time=datetime.now().isoformat() )
-			idat2= dict( value=values[1], type='shinyei-pdp42ns-2.5u', unit='lpo', time=datetime.now().isoformat() )
-			params = urllib.urlencode(dict(postdata=dict( id=self.id, data = [idat1,idat2])))
+			params = urllib.urlencode(dict(postdata=dict( id=self.id, data = idata )))
 			headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
 			conn = httplib.HTTPConnection("sensor.aqicn.org")
 			conn.request("POST", "/sensor/upload/", params, headers)
 			response = conn.getresponse()
-			print("Posting "+str(idat1)+" "+str(idat2)+" -> {0} {1} ".format(response.status, response.reason))
 			data = response.read()
+			print("Posting {2} -> {0} {1} ".format(response.status, response.reason,idata))
 			conn.close()
-			data= json.loads(data)
-			print("Sever says "+str(data))
-			if data["result"] != "ok":
-				self.postponne([idat1,idat2])
+			djson = json.loads(data)
+			r = djson["result"] == "ok"
+			if r!=1:
+				print("Server says -> {0} ".format(data))
+			return r
+		except:
+			e = sys.exc_info()[0]
+			print("http-post: error. "+str(e))
+			return 0 
 
-		except ValueError:  # includes simplejson.decoder.JSONDecodeError
-			print("Data decoding error for "+data)
-			return 0
 
+	def file_get_contents(self,filename):
+		with open(filename) as f:
+			return f.read()
 
-	def postponne(self,values):
-		filename = '/tmp/upload.shinyei.pending.json'
+	def file_put_contents(self,filename,data):
+		with open(filename,'w') as f:
+			f.write(data)
+			f.close()
 
-		obj = []
-		if os.path.isfile(filename):
-			try:
-				with open(filename,'rb') as f:
-				    obj = pickle.load(f)
-			except EOFError:
-				print("pickle error")
-				
-	    obj += values
+	def postValues(self,values):
+		filePath = os.path.join(tempfile.gettempdir(), self.id+".pickle")
+		if self.faildate==0:
+			self.faildate = time.strftime("%Y-%m-%d-%H-%M-%S")
+		filePath2 = os.path.join(os.path.dirname(__file__), "pending."+self.id+"."+self.faildate+".pickle")
 
-		with open(filename, 'wb') as f:
-		    pickle.dump(obj, f)
+		if os.path.isfile(filePath) and os.path.getsize(filePath)>0:
+			ovalues = pickle.loads(self.file_get_contents(filePath))
+			print(ovalues)
+
+			print("previous queue size has "+str(len(ovalues))+" entries")
+			values = ovalues + values
+			os.remove(filePath)
+			print(values)
+
+		if not self.httpPost(values):
+			n = len(values)
+			print("upload not ok... there are now {0} entries pending ({1}).".format(n,filePath))
+			self.file_put_contents(filePath,pickle.dumps(values))
+
+			if n>15:
+				self.writecnt+=1
+				if self.writecnt>10:
+					#only write every 10 times to prevent from wearing the flash
+					self.file_put_contents(filePath2,pickle.dumps(values))
+					print("Writing to persistent storage: "+filePath2)
+					self.writecnt = 0
+
+			if n>1000:
+				print("Persitent storage file is too big ({}) -- reseting to new file ".format(n))
+				self.faildate = 0
+				os.remove(filePath)
+		else:
+			print("Data posting ok!")
+			self.faildate = 0
+			if os.path.isfile(filePath2):
+				print("Deleting persistent storage file "+filePath2)
+				os.remove(filePath2)
+
+class ShinyeiDataUploader(SensorDataUploader):
+
+	def post(self,values):
+		idat1= dict( value=values[0], type='shinyei-pdp42ns-1u', unit='lpo', time=datetime.now().isoformat() )
+		idat2= dict( value=values[1], type='shinyei-pdp42ns-2.5u', unit='lpo', time=datetime.now().isoformat() )
+		self.postValues([idat1,idat2])
+
 
 def loop():
 	reader = ShinyeiLpoReader("gpio7","gpio6")
-	uploader = AqicnUploader("shinyei.test-sensor")
+	uploader = ShinyeiDataUploader("shinyei.test-sensor")
 	while 1:
 		ratio = reader.read(60)
-		print ("--- {0} --- {1} {1} ---".format(time.strftime("%H:%M:%S"),ratio[0],ratio[1]))
-		uploader.post(ratio);
+		print ("--- {0} --- {1} {2} ---".format(time.strftime("%H:%M:%S"),ratio[0],ratio[1]))
+		uploader.post(ratio)
 
 if __name__ == "__main__":
 	loop()
-
 
